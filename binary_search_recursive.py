dc71a21417fd84fa37d22fd168204d235903382c
Iannick Gagnon <38300786+iangagn@users.noreply.github.com>
2021-12-12 17:57:17 -0500

    Update binary_search_recursive.py

--- binary_search_recursive.py
+++ binary_search_recursive.py
@@ -1,6 +1,6 @@
 def binary_search_recursive(arr, low, high, val):
   """
-  Performs recursive binary search on a sorted array. 
+  Performs iterative binary search on a sorted array. 
 
   Parameters
   ----------
@@ -17,16 +17,19 @@ def binary_search_recursive(arr, low, high, val):
     A value to look for in arr. 
   """
 
+  assert isinstance(arr, (list, tuple, set)), \
+         'ARG1 (arr) type must be either list, tuple or set'
+
   if low <= high:
 
       mid = (high + low) // 2
 
       if arr[mid] < val: 
-          return binary_search(arr, mid + 1, high, val) # Ignore left half
+          return binary_search_recursive(arr, mid + 1, high, val) # Ignore left half
       elif arr[mid] > val:
-          return binary_search(arr, low, mid - 1, val)  # Ignore right half
+          return binary_search_recursive(arr, low, mid - 1, val)  # Ignore right half
       else:
-          return mid                                    # Found at mid
+          return mid                                              # Found at mid
 
   else:
       
