7f0c3723a5eecc2344d803b394a46a039dc2e08d
Iannick Gagnon <38300786+iangagn@users.noreply.github.com>
2021-12-12 18:19:25 -0500

    Update binary_search_iterative.c

--- binary_search_iterative.c
+++ binary_search_iterative.c
@@ -1,4 +1,4 @@
-int binary_search_iterative(int* arr,
+int binary_search_iterative(const int* arr,
                             const int arr_size,
                             const int target) 
 {
@@ -28,21 +28,22 @@ int binary_search_iterative(int* arr,
         
         int mid = (low + high) / 2;
         
-        if (arr[mid] < target)
+        if (arr[mid] < target)      // Ignore left half
         {
             low = mid + 1;
         }
-        else if (tab[mid] > target)
+        else if (tab[mid] > target) // Ignore right half
         {
             high = mid - 1;
         }
         else
         {
-            return mid;    
+            return mid;             // Found at mid
         }
         
     }
     
+    // Not found -> return default value
     return -1;
     
 }
